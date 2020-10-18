7abb6cb99afd258d8cdd15dd0506245d2c0fe546
Ahmed Ghumman <ghumman@vt.edu>
2020-10-17 22:05:50 -0600

    Removed unused endpoints from UserController. Added new endpoints. Fixed endpoits And tested all endpoints

--- message/src/main/java/com/friends/message/User/UserController.java
+++ message/src/main/java/com/friends/message/User/UserController.java
@@ -3,6 +3,8 @@ package com.friends.message.User;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -120,8 +122,6 @@ public class UserController {
         }
     }
 
-
-
     @PostMapping("/login")
     public ResponseEntity<UserResponse> login(
         @RequestParam String email, 
@@ -175,91 +175,6 @@ public class UserController {
                     if(passwordMatch) 
                     {
                         userResponse.setMessage("Logged In");
-                        userResponse.setStatus(200);
-                        userResponse.setError(false);
-                        userResponse.setTime(new Timestamp(System.currentTimeMillis()));
-                        return ResponseEntity.ok(userResponse);
-                    } else {
-                        userResponse.setMessage("Login Failed");
-                        userResponse.setStatus(400);
-                        userResponse.setError(true);
-                        userResponse.setTime(new Timestamp(System.currentTimeMillis()));
-                        return ResponseEntity.ok(userResponse);
-                    }
-                } else {
-                    userResponse.setMessage("Authentication Type not right");
-                    userResponse.setStatus(400);
-                    userResponse.setError(true);
-                    userResponse.setTime(new Timestamp(System.currentTimeMillis()));
-                    return ResponseEntity.ok(userResponse);
-                }
-
-            }
-        } catch(Exception e) {
-            userResponse = new UserResponse(); 
-            userResponse.setMessage(e.toString());
-            userResponse.setStatus(400);
-            userResponse.setError(true);
-            userResponse.setTime(new Timestamp(System.currentTimeMillis()));
-            return ResponseEntity.ok(userResponse);
-        }
-    }
-
-    @PostMapping("/delete")
-    public ResponseEntity<UserResponse> delete(
-        @RequestParam String email, 
-        String password, 
-        @RequestParam String authType, 
-        String token
-    ) {
-
-        try {
-
-            userResponse = new UserResponse(); 
-
-            User currentUser = userRepository.findByEmail(email);
-
-            if (currentUser == null) {
-
-                userResponse.setMessage("User Does Not Exist");
-                userResponse.setStatus(400);
-                userResponse.setError(true);
-                userResponse.setTime(new Timestamp(System.currentTimeMillis()));
-                return ResponseEntity.ok(userResponse);
-            }
-
-            else {
-
-                if (authType.equals(AuthType.regular.toString())) {
-
-                    String salt = currentUser.getSalt(); 
-                    String securedPassword = currentUser.getPassword(); 
-                    boolean passwordMatch = PasswordUtils.verifyUserPassword(password, securedPassword, salt);
-            
-                    if(passwordMatch) 
-                    {
-                        userRepository.delete(currentUser);
-                        userResponse.setMessage("User Deleted");
-                        userResponse.setStatus(200);
-                        userResponse.setError(false);
-                        userResponse.setTime(new Timestamp(System.currentTimeMillis()));
-                        return ResponseEntity.ok(userResponse);
-                    } else {
-                        userResponse.setMessage("Login Failed");
-                        userResponse.setStatus(400);
-                        userResponse.setError(true);
-                        userResponse.setTime(new Timestamp(System.currentTimeMillis()));
-                        return ResponseEntity.ok(userResponse);
-                    }
-            
-                } else if (authType.equals(AuthType.special.toString())) {
-                    // login using OAuth
-                    boolean passwordMatch = token.equals(currentUser.getToken());
-            
-                    if(passwordMatch) 
-                    {
-                        userRepository.delete(currentUser);
-                        userResponse.setMessage("User Deleted");
                         userResponse.setStatus(200);
                         userResponse.setError(false);
                         userResponse.setTime(new Timestamp(System.currentTimeMillis()));
@@ -379,15 +294,18 @@ public class UserController {
         @RequestParam String email, 
         HttpServletRequest request
     ) {
+        userResponse = new UserResponse(); 
         User currentUser = userRepository.findByEmail(email); 
 
         if (currentUser == null || currentUser.getAuthType() == User.AuthType.special) {
+
             userResponse.setMessage("No account or logged in using social media");
             userResponse.setStatus(400);
             userResponse.setError(true);
             userResponse.setTime(new Timestamp(System.currentTimeMillis()));
             return ResponseEntity.ok(userResponse);
         } else {
+
             currentUser.setResetToken(UUID.randomUUID().toString());
             userRepository.save(currentUser); 
 
@@ -399,7 +317,7 @@ public class UserController {
 			passwordResetEmail.setTo(currentUser.getEmail());
 			passwordResetEmail.setSubject("Password Reset Request");
 			passwordResetEmail.setText("To reset your password, click the link below:\n" + appUrl
-					+ "/reset?token=" + currentUser.getResetToken());
+					+ ":3000/#/reset-password?token=" + currentUser.getResetToken());
 			
             emailSender.send(passwordResetEmail);
             
@@ -444,11 +362,111 @@ public class UserController {
         }
     }
 
+    @PostMapping(path="/all-friends")
+    public ResponseEntity<UserResponse> allFriends(
+        @RequestParam String email, 
+        String password, 
+        @RequestParam String authType, 
+        String token
+    ) {
+        try {
 
+            userResponse = new UserResponse(); 
 
-    @GetMapping(path="/all")
-    public @ResponseBody Iterable<User> getAllUsers() {
-      // This returns a JSON or XML with the users
-      return userRepository.findAll();
+            User currentUser = userRepository.findByEmail(email);
+
+            if (currentUser == null) {
+
+                userResponse.setMessage("User Does Not Exist");
+                userResponse.setStatus(400);
+                userResponse.setError(true);
+                userResponse.setTime(new Timestamp(System.currentTimeMillis()));
+                return ResponseEntity.ok(userResponse);
+            }
+
+            else {
+
+                if (authType.equals(AuthType.regular.toString())) {
+
+                    String salt = currentUser.getSalt(); 
+                    String securedPassword = currentUser.getPassword(); 
+                    boolean passwordMatch = PasswordUtils.verifyUserPassword(password, securedPassword, salt);
+            
+                    if(passwordMatch) 
+                    {
+                        List<User> users = userRepository.findFriendsWithCustomQuery(currentUser);
+                        List<UsersAll> customUsers = new ArrayList<>(); 
+
+                        for (User user : users) {
+                            UsersAll customUser = new UsersAll(); 
+                            customUser.setEmail(user.getEmail());
+                            customUser.setFirstName(user.getFirstName());
+                            customUser.setLastName(user.getLastName());
+
+                            customUsers.add(customUser); 
+                        }
+
+                        userResponse.setUsersAll(customUsers);
+                        userResponse.setMessage("Friends attached");
+                        userResponse.setStatus(200);
+                        userResponse.setError(false);
+                        userResponse.setTime(new Timestamp(System.currentTimeMillis()));
+                        return ResponseEntity.ok(userResponse);
+                    } else {
+                        userResponse.setMessage("Login Failed");
+                        userResponse.setStatus(400);
+                        userResponse.setError(true);
+                        userResponse.setTime(new Timestamp(System.currentTimeMillis()));
+                        return ResponseEntity.ok(userResponse);
+                    }
+            
+                } else if (authType.equals(AuthType.special.toString())) {
+                    // login using OAuth
+                    boolean passwordMatch = token.equals(currentUser.getToken());
+            
+                    if(passwordMatch) 
+                    {
+                        List<User> users = userRepository.findFriendsWithCustomQuery(currentUser);
+                        List<UsersAll> customUsers = new ArrayList<>(); 
+
+                        for (User user : users) {
+                            UsersAll customUser = new UsersAll(); 
+                            customUser.setEmail(user.getEmail());
+                            customUser.setFirstName(user.getFirstName());
+                            customUser.setLastName(user.getLastName());
+
+                            customUsers.add(customUser); 
+                        }
+
+                        userResponse.setUsersAll(customUsers);
+                        userResponse.setMessage("Friends attached");
+                        userResponse.setStatus(200);
+                        userResponse.setError(false);
+                        userResponse.setTime(new Timestamp(System.currentTimeMillis()));
+                        return ResponseEntity.ok(userResponse);
+                    } else {
+                        userResponse.setMessage("Login Failed");
+                        userResponse.setStatus(400);
+                        userResponse.setError(true);
+                        userResponse.setTime(new Timestamp(System.currentTimeMillis()));
+                        return ResponseEntity.ok(userResponse);
+                    }
+                } else {
+                    userResponse.setMessage("Authentication Type not right");
+                    userResponse.setStatus(400);
+                    userResponse.setError(true);
+                    userResponse.setTime(new Timestamp(System.currentTimeMillis()));
+                    return ResponseEntity.ok(userResponse);
+                }
+
+            }
+        } catch(Exception e) {
+            userResponse = new UserResponse(); 
+            userResponse.setMessage(e.toString());
+            userResponse.setStatus(400);
+            userResponse.setError(true);
+            userResponse.setTime(new Timestamp(System.currentTimeMillis()));
+            return ResponseEntity.ok(userResponse);
+        }
     }
 }
