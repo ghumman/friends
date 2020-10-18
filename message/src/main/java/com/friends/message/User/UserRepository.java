7483ac3297cbf952d86a59fb5e668d81b7376d8f
Ahmed Ghumman <ghumman@vt.edu>
2020-10-17 22:02:44 -0600

    Custom query added in UserRepository to find all users/friends in db except the user which is looking for all users and sorting them by first name and then last name

--- message/src/main/java/com/friends/message/User/UserRepository.java
+++ message/src/main/java/com/friends/message/User/UserRepository.java
@@ -1,10 +1,15 @@
 package com.friends.message.User;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer>{
     User findByEmail(String email);
     Optional<User> findByResetToken(String resetToken);
+
+    @Query("SELECT u FROM User u WHERE u != ?1 order by u.firstName, u.lastName")
+    List<User> findFriendsWithCustomQuery(User user);
 }
