56a542a95cec0a5c7e86f764a0d8dc3640b836b3
Ahmed Ghumman <ghumman@vt.edu>
2020-10-17 22:07:52 -0600

    Added custom query to return the messages sent between two users called findAllWithCustomQuery in MessageRepository interface

--- message/src/main/java/com/friends/message/Message/MessageRepository.java
+++ message/src/main/java/com/friends/message/Message/MessageRepository.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import com.friends.message.User.User;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 public interface MessageRepository extends CrudRepository<Message, Integer>{
     List<Message> findAllByMessageFrom (User messageFrom);
     List<Message> findAllByMessageTo (User messageTo);
-    List<Message> findAllByMessageFromAndMessageTo (User messageFrom, User messageTo);
+
+    @Query("SELECT m FROM Message m WHERE (m.messageFrom = ?1 and m.messageTo = ?2) or (m.messageFrom = ?2 and m.messageTo = ?1) order by m.sentAt")
+    List<Message> findAllWithCustomQuery(User user, User friend);
+
 }
