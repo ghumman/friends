8ec589de3f502da683e6a78b08c4bcd0f8021ae6
Ahmed Ghumman <ghumman@vt.edu>
2020-10-17 22:17:40 -0600

    Fixed and tested Profile.js

--- src/Message/Profile.js
+++ src/Message/Profile.js
@@ -1,10 +1,9 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-
 
 import Button from "react-bootstrap/Button";
 
-
+import Form from "react-bootstrap/Form";
 
 const USERNAME = "username";
 const PASSWORD = "password";
@@ -12,8 +11,51 @@ const LOGIN_TYPE = "login_type";
 const ACCOUNT_TYPE = "account_type";
 const EMAIL = "email";
 
-
 function Profile(props) {
+
+  const [toEmail, setToEmail] = useState("");
+  const [toMessage, setToMessage] = useState("");
+  const [errorServerMessage, setErrorServerMessage] = useState("");
+  const [friends, setFriends] = useState([]);
+  const [friendMessages, setFriendMessages] = useState([]);
+
+
+  const handleChangeToEmail = (event) => {
+    setToEmail(event.target.value);
+  }
+
+  const handleChangeToMessage = (event) => {
+    setToMessage(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch(
+      "http://localhost:8080/send-message",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body:
+          "message=" + toMessage +
+          "&messageFromEmail=" + localStorage.getItem(USERNAME) +
+          "&messageToEmail=" + toEmail.trim() +
+          "&authType=regular" +
+          "&password=" + localStorage.getItem(PASSWORD)
+      }
+    ).then(async function (data) {
+      data.json().then(async function (data) {
+        if (data.message === "Message sent") {
+          setErrorServerMessage("Message sent successfully.");
+        } else {
+          setErrorServerMessage(data.message);
+        }
+      });
+    });
+  }
+
 
   useEffect(() => {
     if (props.authenticate.user.trim() === "") {
@@ -21,7 +63,33 @@ function Profile(props) {
         pathname: "/"
       });
     }
+    getFriends();
   }, [])
+
+  const getFriends = () => {
+    fetch(
+      "http://localhost:8080/all-friends",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body:
+          "email=" + localStorage.getItem(USERNAME) +
+          "&authType=regular" +
+          "&password=" + localStorage.getItem(PASSWORD)
+      }
+    ).then(async function (data) {
+      data.json().then(async function (data) {
+
+        if (data.message === "Friends attached") {
+          setFriends(data.usersAll)
+        } else {
+          setErrorServerMessage(data.message);
+        }
+      });
+    });
+  }
 
   const signMeOut = () => {
     localStorage.setItem(USERNAME, "");
@@ -41,6 +109,32 @@ function Profile(props) {
     });
   }
 
+  const showFriendMessages = (friendEmail) => {
+    fetch(
+      "http://localhost:8080/messages-user-and-friend",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body:
+          "userEmail=" + localStorage.getItem(USERNAME) +
+          "&friendEmail=" + friendEmail +
+          "&authType=regular" +
+          "&password=" + localStorage.getItem(PASSWORD)
+      }
+    ).then(async function (data) {
+      data.json().then(async function (data) {
+
+        if (data.message === "Messages attached") {
+          setFriendMessages(data.msgs)
+        } else {
+          setErrorServerMessage(data.message);
+        }
+      });
+    });
+  }
+
   var changePassword;
   const resultType = localStorage.getItem("login_type");
 
@@ -56,18 +150,117 @@ function Profile(props) {
     );
   }
 
+  var errorMessage;
+  if (errorServerMessage !== "") {
+    errorMessage = (
+      <p
+        style={{
+          paddingTop: 20,
+          color: "red"
+        }}
+      >
+        {errorServerMessage}
+      </p>
+    );
+  } else {
+    errorMessage = null;
+  }
+
+  const friendsData = (
+    <div>
+      {friends.map((usr) =>
+        <div key={usr.email}
+          style={{
+            borderStyle: "solid",
+            borderColor: "white",
+            borderRadius: 5,
+            padding: 5,
+            margin: 5
+          }}>
+          <li onClick={() => showFriendMessages(usr.email)}>
+            {usr.firstName}{' '}{usr.lastName}{' ['}{usr.email}{']'}
+          </li>
+        </div>
+      )}
+    </div>
+  );
+
+  const friendMessagesData = (
+    <div>
+      {friendMessages.map((msg) =>
+        <div key={msg.sentAt}
+          style={{
+            borderStyle: "solid",
+            borderColor: "white",
+            borderRadius: 5,
+            padding: 5,
+            margin: 5
+          }}>
+          <li>
+            From: {'   '}{msg.messageFromEmail}{'    '}To: {'   '}{msg.messageToEmail}
+          </li>
+          <div>
+            {msg.message}
+          </div>
+          <div>
+            {msg.sentAt}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+
   return (
     <div className="App">
       <header className="App-header">
+        {errorMessage}
         <div style={{ padding: 10 }}>
           <Button onClick={() => signMeOut()}>SIGN OUT</Button>
         </div>
         {changePassword}
-        
-        <p>Friends</p>
-        <p>Hi {props.authenticate.user}</p>
 
-        
+        <p>Friends</p>
+        <p>Hi {localStorage.getItem(USERNAME)}</p>
+
+        <p
+          style={{
+            borderStyle: "solid",
+            borderColor: "white",
+            borderRadius: 5,
+            padding: 5
+          }}
+        >
+          Send Message
+          </p>
+
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="toEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              value={toEmail}
+              placeholder="Enter Email"
+              onChange={handleChangeToEmail}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="toMessage">
+            <Form.Label>Message</Form.Label>
+            <Form.Control
+              as="textarea" rows="3"
+              value={toMessage}
+              onChange={handleChangeToMessage}
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            SEND
+            </Button>
+        </Form>
+
+        {friendsData}
+        {friendMessagesData}
+
       </header>
     </div>
   );
