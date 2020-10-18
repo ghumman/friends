228383325b3150be1d52763a0b0265a9e2eaaa82
Ahmed Ghumman <ghumman@vt.edu>
2020-10-17 22:03:41 -0600

    Unused import removed from SpringFoxConfig

--- message/src/main/java/com/friends/message/SpringFoxConfig.java
+++ message/src/main/java/com/friends/message/SpringFoxConfig.java
@@ -27,7 +27,6 @@ import com.friends.message.User.UserController;
 // import org.joda.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpMethod;
