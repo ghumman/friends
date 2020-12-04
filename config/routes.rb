b2dc428efa6f136ce4c7a3d39577184b9ff1c783
Ahmed Ghumman <ghumman@vt.edu>
2020-12-04 07:20:37 -0700

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/add-user', to: 'users#addUser'
-  get '/login', to: 'users#login'
-  get '/change-password', to: 'users#changePassword'
-  get '/forgot-password', to: 'users#forgotPassword'
-  get '/reset-password', to: 'users#resetPassword'
-  get '/all-friends', to: 'users#allFriends'
+  post '/add-user', to: 'users#addUser'
+  post '/login', to: 'users#login'
+  post '/change-password', to: 'users#changePassword'
+  post '/forgot-password', to: 'users#forgotPassword'
+  post '/reset-password', to: 'users#resetPassword'
+  post '/all-friends', to: 'users#allFriends'
 
-  get '/send-message', to: 'messages#sendMessage'
-  get '/messages-user-and-friend', to: 'messages#messagesUserAndFriend'
+  post '/send-message', to: 'messages#sendMessage'
+  post '/messages-user-and-friend', to: 'messages#messagesUserAndFriend'
 end
