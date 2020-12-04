1f9514d063edf1efd37e83e91899be2532ac6f90
Ahmed Ghumman <ghumman@vt.edu>
2020-12-04 07:21:14 -0700

    Moved common methods in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+    def generateKey (password, salt)
+        return OpenSSL::PKCS5.pbkdf2_hmac(password.encode(Encoding::ASCII_8BIT), salt.encode(Encoding::ASCII_8BIT), 10000, 32, "sha1")
+    end
+
 end
