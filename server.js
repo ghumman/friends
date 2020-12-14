0e03b05baf57c2e703bac1e58c5e5578f5f4c2d8
Ahmed Ghumman <ghumman@vt.edu>
2020-12-14 13:02:31 -0700

    Added Access-Control-Allow-Origin for angular app

--- server.js
+++ server.js
@@ -30,6 +30,16 @@ con.connect(function(err) {
   if (err) throw err;
 });
 
+app.use(function (req, res, next) {
+
+	    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+	    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	    res.setHeader('Access-Control-Allow-Credentials', true);
+
+	    next();
+});
+
 // User Related Endpoints
 // add-user
 app.post('/add-user', urlencodedParser, function (req, res) {
