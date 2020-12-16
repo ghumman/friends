c3fa7c4100034b4bd849e66b63e70a857e57b087
Ahmed Ghumman <ghumman@vt.edu>
2020-12-16 07:39:02 -0700

    Forgot screen working

--- src/app/forgot/forgot.component.ts
+++ src/app/forgot/forgot.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot',
@@ -7,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotComponent implements OnInit {
 
-  constructor() { }
+  form;
+  errorMessage;
+  email;
+  password;
+
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {
+    this.email = localStorage.getItem('email');
+    this.password = localStorage.getItem('password');
+
+    this.form = this.formBuilder.group({
+      email: ''
+    })
+   }
 
   ngOnInit(): void {
+    if (this.email !== null && this.password !== null) {
+      this.router.navigateByUrl('/profile');
+    }
+  }
+
+  goToLogin() {
+    this.router.navigate(['/']);
+  }
+
+
+  onSubmit(customerData) {
+
+    this.email = customerData.email.trim();
+
+    const that = this;
+    fetch(
+      "http://localhost:8080/forgot-password",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: "email=" + this.email
+      }
+    ).then(async function(data) {
+      data.json().then(async function(data) {
+        if (data.message === "Reset password is sent") {
+          that.errorMessage = "Password reset link is sent to your email.";
+        } else {
+          that.errorMessage =  data.message;
+        }
+      });
+    });
   }
 
 }
