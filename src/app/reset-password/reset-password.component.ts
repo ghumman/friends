24afb9d87132dc2b81d78ed2a6db1d630b9ab9bb
Ahmed Ghumman <ghumman@vt.edu>
2020-12-16 09:02:18 -0700

    Reset Password screen is working

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +9,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordComponent implements OnInit {
 
-  constructor() { }
+  form;
+  errorMessage;
+  email;
+  password;
+  passwordConfirm;
+  token;
+
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { 
+    this.email = localStorage.getItem('email');
+    this.password = localStorage.getItem('password');
+
+    this.form = this.formBuilder.group({
+      email: '',
+      password: '',
+      passwordConfirm: ''
+    })
+  }
 
   ngOnInit(): void {
+    if (this.email !== null && this.password !== null) {
+      this.router.navigateByUrl('/profile');
+    }
+
+    this.token = this.router.parseUrl(this.router.url).queryParams.token;
+    if (this.token !== null && this.token !== undefined && this.token !== "") {
+      this.errorMessage = "Please select new password.";
+    } else {
+      this.errorMessage = "Url is tempered";
+    }
+
+  }
+
+
+  goToLogin() {
+    this.router.navigate(['/']);
+  }
+
+  onSubmit(customerData) {
+    this.password = customerData.password.trim();
+    this.passwordConfirm = customerData.passwordConfirm.trim();
+
+    const that = this;
+
+    if (this.password.length >= 5 && this.password === this.passwordConfirm) {
+
+      fetch(
+        "http://localhost:8080/reset-password",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          body:
+            "password=" +
+            this.password +
+            "&token=" +
+            this.token
+        }
+      ).then(async function(data) {
+        data.json().then(async function(data) {
+          if (data.message === "Password successfully reset") {
+            that.errorMessage = "Password updated successfully.";
+          } else {
+            that.errorMessage = data.message;
+
+          }
+        });
+      });
+    } else {
+      this.errorMessage = "Password should be 5 characters long and match.";
+    }
   }
 
 }
