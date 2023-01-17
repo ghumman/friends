1a06c3300c98e32f70a631758b311367e883d25c
Ahmed Ghumman <ahmed.ghumman@realogy.com>
2023-01-17 15:06:29 -0700

    Updates

---
+++ precache-manifest.46c23139856ac6a9c7c6a5c206eeb108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90796062a1516d28b2eb5d6cb75b33be",
+    "url": "/friends/index.html"
+  },
+  {
+    "revision": "f8965cb1a7c64b807464",
+    "url": "/friends/static/css/main.7834e9f0.chunk.css"
+  },
+  {
+    "revision": "d5f185e6cf0cc95d7704",
+    "url": "/friends/static/js/2.98309d42.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/friends/static/js/2.98309d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8965cb1a7c64b807464",
+    "url": "/friends/static/js/main.57e5b0a1.chunk.js"
+  },
+  {
+    "revision": "fed8cae69042b80d258b",
+    "url": "/friends/static/js/runtime-main.6ad2189b.js"
+  }
+]);
